72e4e2ea65749ce4de03651205307b048fbc9dc1
Nagae Hidetake <nagae@eagan.jp>
2023-01-03 11:46:35 +0900

    AutomatonSetting

---
+++ AutomatonSetting.js
@@ -0,0 +1,381 @@
+var ids = {
+    head: {
+	q: "h",
+	name: "#name_head",
+	element: [
+	    "#element0head",
+	    "#element1head",
+	    "#element2head",
+	    "#element3head",
+	    "#element4head",
+	    "#element5head",
+	    "#element6head",
+	    "#element7head"
+	]
+    },
+    frame: {
+	q: "f",
+	name: "#name_frame",
+	element: [
+	    "#element0frame",
+	    "#element1frame",
+	    "#element2frame",
+	    "#element3frame",
+	    "#element4frame",
+	    "#element5frame",
+	    "#element6frame",
+	    "#element7frame"
+	]
+    },
+    attachment: [
+	{
+	    q: "a00",
+	    name: "#name_a00",
+	    element: [
+		"#element0a00",
+		"#element1a00",
+		"#element2a00",
+		"#element3a00",
+		"#element4a00",
+		"#element5a00",
+		"#element6a00",
+		"#element7a00"
+	    ]
+	},
+	{
+	    q: "a01",
+	    name: "#name_a01",
+	    element: [
+		"#element0a01",
+		"#element1a01",
+		"#element2a01",
+		"#element3a01",
+		"#element4a01",
+		"#element5a01",
+		"#element6a01",
+		"#element7a01"
+	    ]
+	},
+	{
+	    q: "a02",
+	    name: "#name_a02",
+	    element: [
+		"#element0a02",
+		"#element1a02",
+		"#element2a02",
+		"#element3a02",
+		"#element4a02",
+		"#element5a02",
+		"#element6a02",
+		"#element7a02"
+	    ]
+	},
+	{
+	    q: "a03",
+	    name: "#name_a03",
+	    element: [
+		"#element0a03",
+		"#element1a03",
+		"#element2a03",
+		"#element3a03",
+		"#element4a03",
+		"#element5a03",
+		"#element6a03",
+		"#element7a03"
+	    ]
+	},
+	{
+	    q: "a04",
+	    name: "#name_a04",
+	    element: [
+		"#element0a04",
+		"#element1a04",
+		"#element2a04",
+		"#element3a04",
+		"#element4a04",
+		"#element5a04",
+		"#element6a04",
+		"#element7a04"
+	    ]
+	},
+	{
+	    q: "a05",
+	    name: "#name_a05",
+	    element: [
+		"#element0a05",
+		"#element1a05",
+		"#element2a05",
+		"#element3a05",
+		"#element4a05",
+		"#element5a05",
+		"#element6a05",
+		"#element7a05"
+	    ]
+	},
+	{
+	    q: "a06",
+	    name: "#name_a06",
+	    element: [
+		"#element0a06",
+		"#element1a06",
+		"#element2a06",
+		"#element3a06",
+		"#element4a06",
+		"#element5a06",
+		"#element6a06",
+		"#element7a06"
+	    ]
+	},
+	{
+	    q: "a07",
+	    name: "#name_a07",
+	    element: [
+		"#element0a07",
+		"#element1a07",
+		"#element2a07",
+		"#element3a07",
+		"#element4a07",
+		"#element5a07",
+		"#element6a07",
+		"#element7a07"
+	    ]
+	},
+	{
+	    q: "a08",
+	    name: "#name_a08",
+	    element: [
+		"#element0a08",
+		"#element1a08",
+		"#element2a08",
+		"#element3a08",
+		"#element4a08",
+		"#element5a08",
+		"#element6a08",
+		"#element7a08"
+	    ]
+	},
+	{
+	    q: "a09",
+	    name: "#name_a09",
+	    element: [
+		"#element0a09",
+		"#element1a09",
+		"#element2a09",
+		"#element3a09",
+		"#element4a09",
+		"#element5a09",
+		"#element6a09",
+		"#element7a09"
+	    ]
+	},
+	{
+	    q: "a10",
+	    name: "#name_a10",
+	    element: [
+		"#element0a10",
+		"#element1a10",
+		"#element2a10",
+		"#element3a10",
+		"#element4a10",
+		"#element5a10",
+		"#element6a10",
+		"#element7a10"
+	    ]
+	},
+	{
+	    q: "a11",
+	    name: "#name_a11",
+	    element: [
+		"#element0a11",
+		"#element1a11",
+		"#element2a11",
+		"#element3a11",
+		"#element4a11",
+		"#element5a11",
+		"#element6a11",
+		"#element7a11"
+	    ]
+	},
+	{
+	    q: "a12",
+	    name: "#name_a12",
+	    element: [
+		"#element0a12",
+		"#element1a12",
+		"#element2a12",
+		"#element3a12",
+		"#element4a12",
+		"#element5a12",
+		"#element6a12",
+		"#element7a12"
+	    ]
+	},
+	{
+	    q: "a13",
+	    name: "#name_a13",
+	    element: [
+		"#element0a13",
+		"#element1a13",
+		"#element2a13",
+		"#element3a13",
+		"#element4a13",
+		"#element5a13",
+		"#element6a13",
+		"#element7a13"
+	    ]
+	},
+	{
+	    q: "a14",
+	    name: "#name_a14",
+	    element: [
+		"#element0a14",
+		"#element1a14",
+		"#element2a14",
+		"#element3a14",
+		"#element4a14",
+		"#element5a14",
+		"#element6a14",
+		"#element7a14"
+	    ]
+	},
+	{
+	    q: "a15",
+	    name: "#name_a15",
+	    element: [
+		"#element0a15",
+		"#element1a15",
+		"#element2a15",
+		"#element3a15",
+		"#element4a15",
+		"#element5a15",
+		"#element6a15",
+		"#element7a15"
+	    ]
+	}
+    ],
+    sum: {
+	element: [
+	    "#element0sum",
+	    "#element1sum",
+	    "#element2sum",
+	    "#element3sum",
+	    "#element4sum",
+	    "#element5sum",
+	    "#element6sum",
+	    "#element7sum"
+	]
+    },
+    capacity: {
+	element: [
+	    "#element0cap",
+	    "#element1cap",
+	    "#element2cap",
+	    "#element3cap",
+	    "#element4cap",
+	    "#element5cap",
+	    "#element6cap",
+	    "#element7cap"
+	]
+    }
+};
+
+var automaton;
+
+function clear(lineids) {
+    var i;
+    $(lineids.name).text("");
+    for (i in lineids.element) {
+	$(lineids.element[i]).text("");
+    }
+}
+
+function refresh() {
+    var i, j, p;
+    var capacity = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
+    var sum = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
+    var params = new URL(document.location).searchParams;
+
+    var head = null;
+    var frame = null;
+    var attachment = [];
+
+
+    p = params.get(ids.head.q);
+    if (p) {
+	for (i in automaton.head) {
+	    if (automaton.head[i].id == p) {
+		head = automaton.head[i];
+		for (j in capacity) {
+		    capacity[j] += head.capacity[j];
+		}
+	    }
+	}
+    }
+    p = params.get(ids.frame.q);
+    if (p) {
+	for (i in automaton.frame) {
+	    if (automaton.frame[i].id == p) {
+		frame = automaton.frame[i];
+		for (j in capacity) {
+		    capacity[j] += frame.capacity[j];
+		}
+	    }
+	}
+    }
+    for (j in ids.attachment) {
+	p = params.get(ids.attachment[j].q);
+	if (p) {
+	    for (i in automaton.attachment) {
+		if (automaton.attachment[i].id == p) {
+		    attachment[j] = automaton.attachment[i];
+		    sum[attachment[j].element] += attachment[j].weight;
+		}
+	    }
+	}
+    }
+	
+    clear(ids.head);
+    if (head) {
+	$(ids.head.name).text(head.name.ja)
+	for (i in ids.head.element) {
+	    $(ids.head.element[i]).text(head.capacity[i]);
+	}
+    }
+    clear(ids.frame);
+    if (frame) {
+	$(ids.frame.name).text(frame.name.ja)
+	for (i in ids.frame.element) {
+	    $(ids.frame.element[i]).text(frame.capacity[i]);
+	}
+    }
+    for (i in ids.attachment) {
+	clear(ids.attachment[i]);
+	if (attachment[i]) {
+	    $(ids.attachment[i].name).text(attachment[i].name.ja);
+	    $(ids.attachment[i].element[attachment[i].element]).text(attachment[i].weight);
+	}
+    }
+    for (i in ids.capacity.element) {
+	$(ids.capacity.element[i]).text(capacity[i])
+    }
+    for (i in ids.sum.element) {
+	$(ids.sum.element[i]).text(sum[i])
+	if (capacity[i] < sum[i]) {
+	    $(ids.sum.element[i]).parent().addClass("alert");
+	} else {
+	    $(ids.sum.element[i]).parent().removeClass("alert");
+	}
+    }
+}
+
+function setup() {
+    $.ajax({
+	url: "data/automaton.js",
+	dataType: "json",
+	method: "GET",
+	success: function(data, status, handler) {
+	    automaton = data;
+	    refresh();
+	}
+    });
+}
